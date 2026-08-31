4810b88b1ec3dc4e1aed1836fd105141c6884bb3
local_devkimqwer <dev.kim.qwer@gmail.com>
2026-08-31 16:41:08 +0900

    서버 액션 업로드 용량 제한 상향

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.*.*"],
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "50mb",
+    },
+  },
   images: {
     remotePatterns: [
       {
